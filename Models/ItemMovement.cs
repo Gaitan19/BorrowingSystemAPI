2b577c500454a1a89a3b64592115c1377fd7c296
kenley <gaitan19>
2025-03-28 16:07:47 -0600

    Add initial models and database context for BorrowingSystemAPI

---
+++ Models/ItemMovement.cs
@@ -0,0 +1,19 @@
+﻿namespace BorrowingSystemAPI.Models
+{
+    public enum MovementType
+    {
+        Entry,
+        Exit
+    }
+
+    public class ItemMovement
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid ItemId { get; set; }
+        public Item Item { get; set; }
+        public DateTime MovementDate { get; set; } = DateTime.UtcNow;
+        public MovementType MovementType { get; set; }
+        public int Quantity { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
